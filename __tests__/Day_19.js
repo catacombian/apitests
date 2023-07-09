85a162603fe83cd7f8510bd41b5dc5443aa8f2f4
Elena Mangutova <137103324+catacombian@users.noreply.github.com>
2023-07-09 21:20:59 +0400

    Day 19 fixed

--- __tests__/Day_19.js
+++ __tests__/Day_19.js
@@ -1,40 +1,32 @@
-const frisby = require('frisby');
-const Joi = frisby.Joi;
+const frisby = require("frisby");
 
-describe('Postman Day 19', () => {
-  it('Get Artist', async () => {
-    const query = `query getByArtist($name: String!) {
+describe("Day 19", () => {
+  it("Get Artist Information", async () => {
+    let variables = {
+      name: 'Red Hot Chili Peppers',
+    };
+    let query = `query getByArtist($name: String!) {
       queryArtists(byName: $name) {
-        name
-        image
-        albums {
           name
-        }
-        name
-        image
-        albums {
-          name
-        }
+          image
+          albums {
+              name
+          }
       }
     }`;
 
-    const artist = {
-      name: 'Red Hot Chili Peppers',
-    };
-
     const response = await frisby
       .post('https://joyce-spotify-graphql.herokuapp.com/graphql', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
+
         body: JSON.stringify({
           query,
           variables
         })
       })
       .expect('status', 200)
-      .expect('json', 'data.queryArtists[0].name', `${artist.name}`);
+      .expect('json', 'data.queryArtists[0].name', `${variables.name}`);
 
     console.log(response.json.data);
   });
-});
+})
+
