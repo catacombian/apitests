b3fbada5b140cdb852b3d7fea07f075cac678dad
Elena Mangutova <137103324+catacombian@users.noreply.github.com>
2023-07-10 15:52:57 +0400

    28

--- __tests__/Day_28.js
+++ __tests__/Day_28.js
@@ -2,31 +2,14 @@ const URL = 'https://www.postman.com/';
 const frisby = require('frisby');
 
 jest.setTimeout(15000);
-
-// describe('Day 28', () => {
-//   it('UI', async () => {
-//     const response = await frisby.get(URL).expect('status', 200);
-
-//     if (response._responseTimeMs < 1000) {
-//       console.log('Response time is less than 1000ms. Test passed!');
-//     } else {
-//       console.log(
-//         'Response time is greater than or equal to 1000ms. Test failed!'
-//       );
-//     }
-//   });
-
-  it('Lighthouse',  () => {
-    const response = frisby
-      .get(
-        `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${URL}`
-      )
-      .expect('status', 200)
-      .expect('json', {
-        data: {
-          serverResponseTime: '46',
-        }
-      });
-    });
-
-
+it('Lighthouse',  () => {
+  frisby.get(
+    `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${URL}`
+  )
+  .expect('status', 200)
+  .expect('json', {
+    data: {
+      serverResponseTime: '46',
+    }
+  });
+});
