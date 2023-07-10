78f0201b8a7b3c4bb2a37043899c33a4ddd6dc26
Elena Mangutova <137103324+catacombian@users.noreply.github.com>
2023-07-10 22:27:55 +0400

    Day 23 timeout fixed

--- __tests__/Day_23.js
+++ __tests__/Day_23.js
@@ -1,23 +1,25 @@
 const frisby = require('frisby');
 const Joi = frisby.Joi;
-const URL = 'https://postman-echo.com';
-
+const URL = 'https://postman-echo.com/get?';
+jest.setTimeout(500000);
 const fs = require('fs');
 const path = require('path');
+const directoryName = path.basename(__dirname);
+const filePath = path.resolve(directoryName, '../', 'geoMap.csv');
+const fileData = fs.readFileSync(filePath, 'utf8');
+const table = fileData;
 
 describe('Postman Day 23', () => {
-  // jest.setTimeout(100000);
+
   it('Geo Map', async () => {
-    const directoryName = path.basename(__dirname);
-    const filePath = path.resolve(directoryName, '../', 'geoMap.csv');
-    process.cwd()
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    const table = fileData.split('\n');
+
     console.log(table);
 
     for (let i = 1; i < table.length; i++) {
       const [region, boba] = table[i].split(',');
-      const response = await frisby.get(`${URL}/get?${region}=${boba}`);
+      const response = await frisby.get(`${URL}${region}=${boba}`);
     }
   });
 });
+
+
