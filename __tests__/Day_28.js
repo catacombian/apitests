000de7961680619b433b9c6a4956a19146afe4c9
Elena Mangutova <137103324+catacombian@users.noreply.github.com>
2023-07-12 00:17:48 +0400

    Day 28 hidden

--- __tests__/Day_28.js
+++ __tests__/Day_28.js
@@ -1,19 +1,19 @@
-// const URL = 'https://www.postman.com/';
-// const frisby = require('frisby');
+const URL = 'https://www.postman.com/';
+const frisby = require('frisby');
 
 // jest.setTimeout(15000);
 
 
-// describe('Day 28', () => {
-// it('Lighthouse',  () => {
-//   frisby.get(
-//     `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${URL}`
-//   )
-//   .expect('status', 200)
-//   .expect('json', {
-//     data: {
-//       serverResponseTime: '46',
-//     }
-//   });
-// });
-// })
+describe('Day 28', () => {
+it('Lighthouse',  () => {
+  frisby.get(
+    `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${URL}`
+  )
+  .expect('status', 200)
+  .expect('json', {
+    // data: {
+    //   serverResponseTime: '46',
+    // }
+  });
+});
+})
