eb59d7091b2827ad92e6e137892b431e476effbe
Elena Mangutova <137103324+catacombian@users.noreply.github.com>
2023-07-12 00:54:59 +0400

    Day 7 fixed

--- __tests__/Day_07.js
+++ __tests__/Day_07.js
@@ -1,20 +1,25 @@
-const { response } = require('express');
-const frisby = require('frisby');
+const frisby = require("frisby");
 const Joi = frisby.Joi;
-const coindeskBaseUrl = 'https://api.coindesk.com'
+frisby.globalSetup({
+	request: {
+		timeout: 10000,
+	},
+});
 
-describe('Postman day 7', () => {
-  it('Get pics', function () {
-    return frisby
-    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=10')
-    .expect('status', 200)
-    .then((response) => {
-      const getpics = response.json.map((item) => ({
-        title: item.title,
-        url: item.url,
-      } 
-    )); 
-    console.log(getpics);
-    });
-  }) 
-})
+// const { response } = require('express');
+
+
+describe('Day 7', () => {
+  it('Apod', async () => {
+    const response = await frisby
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=3")
+		.expect("status", 200)
+		.expect('jsonTypes', '*', {
+	        title: Joi.string(),
+	        url: Joi.string(),})
+		.then((result) => {
+			let pics = result.json
+				pics.forEach((pic) => {console.log(pic.title, pic.url)})
+			});
+		});
+});	
