b06a8683b5b4d3506fc1e47e1082bad293e15517
Elena Mangutova <137103324+catacombian@users.noreply.github.com>
2023-07-11 13:13:14 +0400

    Day 14

--- __tests__/Day_14.js
+++ __tests__/Day_14.js
@@ -1,6 +1,6 @@
 const frisby = require('frisby');
 const Joi = frisby.Joi;
-const AuthorizationToken = process.env.AuthorizationToken
+const AuthorizationToken = process.env.AUTHORIZATIONTOKEN
 describe('Postman Day 14', () => {
   it('Github OAuth', async () => {
     const response = await frisby
@@ -13,5 +13,3 @@ describe('Postman Day 14', () => {
   });
 })
 
-// AuthorizationToken=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
-// // 7f54302cc8db1c10e218de551e9ad86a8699c770
